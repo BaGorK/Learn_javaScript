d3fae1fd68928fca76c43a99ccb6cd036d71f5fc
BaGorK <edmealemkassahun@gmail.com>
2023-07-27 00:59:50 -0700

    Running Promises in Parallel

--- 16-Asynchronous-JS/script.js
+++ 16-Asynchronous-JS/script.js
@@ -219,14 +219,14 @@ const renderError = function (msg) {
 // };
 
 ///////////////////////////////////////////////////////////
-// const getJSON = function (url, errorMsg) {
-//   return fetch(url).then(response => {
-//     if (!response.ok) {
-//       throw new Error(errorMsg);
-//     }
-//     return response.json();
-//   });
-// };
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(errorMsg);
+    }
+    return response.json();
+  });
+};
 
 // const getCountryData = function (country) {
 //   getJSON(`https://restcountries.com/v3.1/name/${country}`, `Country not found`)
@@ -358,23 +358,19 @@ GOOD LUCK 😀
 //     console.log(response);
 //   })
 //   .catch(err => console.log(err));
-  
 
-  
-  //         setTimeout(() => {
-  //           console.log('5 second passed');
-  //           setTimeout(() => {
-  //             console.log('6 seconds passed');
-  //             setTimeout(() => {
-  //               console.log('7 second passed');
-  //               setTimeout(() => {
-  //                 console.log('8 seconds passed');
-  //               }, 1000);
-  //             }, 1000);
-  //           }, 1000);
-  //         }, 1000);
-
-
+//         setTimeout(() => {
+//           console.log('5 second passed');
+//           setTimeout(() => {
+//             console.log('6 seconds passed');
+//             setTimeout(() => {
+//               console.log('7 second passed');
+//               setTimeout(() => {
+//                 console.log('8 seconds passed');
+//               }, 1000);
+//             }, 1000);
+//           }, 1000);
+//         }, 1000);
 
 //   // Promisifying setTimeout
 //   const wait = second =>
@@ -400,57 +396,79 @@ GOOD LUCK 😀
 //   })
 //   .then(() => console.log('5 second passed'));
 
+// //////////////////////////////////////////////
+// // Async_Await
+// const getPosition = function () {
+//   return new Promise(function (resolve, reject)) {
+//     navigator.geolocation.getCurrentPosition(resolve, reject);
+//   }
+// }
+// const whereAmI = async function () {
+//   try {
+//     const pos = await getPosition();
+//     const { latitude: lat, longitude: lng } = pos.coords;
+//     const responseGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+//     if (!responseGeo.ok) throw new Error('Problem getting location data');
+//     const dataGeo = await responseGeo.json();
 
+//     const response = await fetch(`https://restcountries.com/v3.1/name/${dataGeo.country}`);
+//     if(!response.ok) throw new Error('Problem getting Country')
+//     console.log(response);
+//     const data = await response.json();
+//     console.log(data);
+//     renderCountryData(data[0])
+//     return `You are in ${dataGeo.city}, ${dataGeo.country}`
+//   } catch (error) {
+//     console.log(`${error}💥💥💥`);
+//     renderError(`Something went Wrong ${err.message}`)
 
-//////////////////////////////////////////////
-// Async_Await
-const getPosition = function () {
-  return new Promise(function (resolve, reject)) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  }
-}
-const whereAmI = async function () {
-  try {    
-    const pos = await getPosition();
-    const { latitude: lat, longitude: lng } = pos.coords;
-    const responseGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    if (!responseGeo.ok) throw new Error('Problem getting location data');
-    const dataGeo = await responseGeo.json();
-  
-    const response = await fetch(`https://restcountries.com/v3.1/name/${dataGeo.country}`);
-    if(!response.ok) throw new Error('Problem getting Country')
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
-    renderCountryData(data[0])
-    return `You are in ${dataGeo.city}, ${dataGeo.country}`
-  } catch (error) {
-    console.log(`${error}💥💥💥`);
-    renderError(`Something went Wrong ${err.message}`)
+//     // Reject Promise returned from async funtion
+//     throw err;
+//   }
+// }
+// // const city = whereAmI()
+// // // an async function always returns a promise
+// // console.log(city);
+// whereAmI()
+//   .then(city => console.log(`2: ${city}`))
+//   .catch(err => console.error(`2: ${err.message} 💥💥💥`))
+//   .finally(() => console.log('3: Finished getting location'));
+// // Even though there is an error in an async function, the promise that this async functions is still fullfilled and not rejected.
 
-    // Reject Promise returned from async funtion
-    throw err;
-  }
-}
-// const city = whereAmI()
-// // an async function always returns a promise
-// console.log(city); 
-whereAmI()
-  .then(city => console.log(`2: ${city}`))
-  .catch(err => console.error(`2: ${err.message} 💥💥💥`))
-  .finally(() => console.log('3: Finished getting location'));
-// Even though there is an error in an async function, the promise that this async functions is still fullfilled and not rejected.
+// // we can convert the above one to an Immediatly invoked function expression
 
+// (async function () {
+//   try {
+//     const city = await whereAmI();
+//     console.log(`2: ${city}`);
+//   } catch (err) {
+//     console.error(`2: ${err.message} 💥💥💥`);
+//   }
+//   console.log('3: Finished getting location');
+// })(); // here we get the exact same result as the above;
 
-// we can convert the above one to an Immediatly invoked function expression
-
-(async function () {
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+// Running Promises in Parallel
+const get3Countries = async function (c1, c2, c3) {
   try {
-    const city = await whereAmI();
-    console.log(`2: ${city}`);
-  } catch (err) {
-    console.error(`2: ${err.message} 💥💥💥`);
-  }
-  console.log('3: Finished getting location');
-})(); // here we get the exact same result as the above;
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    // // these above, run sequentially
+    // console.log([data1.capital], [data2.capital], [data3.capital]);
 
+    const data = await Promise.all(
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`)
+    );
+    // Promise.all() -- receives an array and also returns an array also
+// Notice: if one of the promises reject, then the whole promise at all actually rejects as well
+    console.log(data.map(d => d[0].capital));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
