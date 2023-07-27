d161b44a7987cdb7fec05810e4ee2de9445bacf9
BaGorK <edmealemkassahun@gmail.com>
2023-07-27 04:50:41 -0700

    Promise Combinators: Promise.raceE([..])

--- 16-Asynchronous-JS/script.js
+++ 16-Asynchronous-JS/script.js
@@ -450,25 +450,74 @@ GOOD LUCK 😀
 ///////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
 // Running Promises in Parallel
-const get3Countries = async function (c1, c2, c3) {
-  try {
-    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
-    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
-    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
-    // // these above, run sequentially
-    // console.log([data1.capital], [data2.capital], [data3.capital]);
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+//     // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+//     // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+//     // // these above, run sequentially
+//     // console.log([data1.capital], [data2.capital], [data3.capital]);
 
-    const data = await Promise.all(
-      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c3}`)
-    );
-    // Promise.all() -- receives an array and also returns an array also
-// Notice: if one of the promises reject, then the whole promise at all actually rejects as well
-    console.log(data.map(d => d[0].capital));
-  } catch (error) {
-    console.log(error);
-  }
+//     const data = await Promise.all(
+//       getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c3}`)
+//     );
+//     // Promise.all() -- receives an array and also returns an array also
+// // Notice: if one of the promises reject, then the whole promise at all actually rejects as well
+//     console.log(data.map(d => d[0].capital));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// get3Countries('portugal', 'canada', 'tanzania');
+
+///////////////////////////////////////
+// Other Promise Combinators: race, allSettled and any
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.eu/rest/v2/name/italy`),
+    getJSON(`https://restcountries.eu/rest/v2/name/egypt`),
+    getJSON(`https://restcountries.eu/rest/v2/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long!'));
+    }, sec * 1000);
+  });
 };
 
-get3Countries('portugal', 'canada', 'tanzania');
+Promise.race([
+  getJSON(`https://restcountries.eu/rest/v2/name/tanzania`),
+  timeout(5),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+//////////////////////////////////////////////////////////
+
+// a utility for timing out a promise
+const timeoutPromise = function (delay) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      reject('TimeOut');
+    }, delay);
+  });
+};
+
+// Setup a timeout for 'foo()'
+Promise.race([foo(), timeoutPromise(3000)]).then(
+  function () {
+    // 'foo(..)' fulfilled in time!
+  },
+  function (err) {
+    // either `foo()` rejected, or it just
+    // didn't finish in time, so inspect
+    // `err` to know which
+  }
+);
