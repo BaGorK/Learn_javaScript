95cff3ea262dfb89089b8d336b153d85b6ceeb01
BaGorK <edmealemkassahun@gmail.com>
2023-08-02 10:27:50 -0700

    basics of constructor functions and the new Operator

--- 14-Object-Oriented-Programming/script.js
+++ 14-Object-Oriented-Programming/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// an arrow function does not work as a constructor function
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+    this.birthYear = birthYear;
+    
+    // it is a bad practice to create a method on a constructor function
+    // this.calcAge = function () {
+    //     console.log(2037 - this.birthYear);
+    // }
+};
+
+const Bagor = new Person('Edmealem', 1993);
+
+// is Instanceof : boolean
+console.log(Bagor instanceof Person);
+
