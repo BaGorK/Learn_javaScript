1081850dae12d216802f5302195008f55950c052
BaGorK <edmealemkassahun@gmail.com>
2023-08-02 10:48:34 -0700

    Prototypes in practice

--- 14-Object-Oriented-Programming/script.js
+++ 14-Object-Oriented-Programming/script.js
@@ -3,12 +3,12 @@
 // an arrow function does not work as a constructor function
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
-    this.birthYear = birthYear;
-    
-    // it is a bad practice to create a method on a constructor function
-    // this.calcAge = function () {
-    //     console.log(2037 - this.birthYear);
-    // }
+  this.birthYear = birthYear;
+
+  // it is a bad practice to create a method on a constructor function
+  // this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  // }
 };
 
 const Bagor = new Person('Edmealem', 1993);
@@ -16,3 +16,24 @@ const Bagor = new Person('Edmealem', 1993);
 // is Instanceof : boolean
 console.log(Bagor instanceof Person);
 
+/////////////////////////////////////////////////
+// Prototypes
+
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2015 - this.birthYear);
+};
+
+// NOTE: objects which are instances of the Person class will get access to all the methods of this Prototype property 
+
+Bagor.calcAge();
+
+console.log(Bagor.__proto__);
+console.log(Bagor.__proto__ === Person.prototype); // true
+
+console.log(Person.prototype.isPrototypeOf(Bagor)); // true
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+Person.prototype.species = 'Homo Sapiens'
+console.log(Bagor.species);
