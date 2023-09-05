44f02ab75f5e4a6a68c1c468d6a037dec4d8b0e5
BaGorK <edmealemkassahun@gmail.com>
2023-09-04 22:46:10 -0700

    coding challenge on function constructors

--- 14-Object-Oriented-Programming/script.js
+++ 14-Object-Oriented-Programming/script.js
@@ -41,6 +41,7 @@ Person.prototype.calcAge = function () {
 Bagor.calcAge(); // the this keyword is set to the object that is calling the method. this = Bagor
 
 // console.log(Bagor.__proto__);
+//- The prototype of the bagor object(instance) is essensialy the prototype property of the constructor function.
 // console.log(Bagor.__proto__ === Person.prototype); // true
 
 // console.log(Person.prototype.isPrototypeOf(Bagor)); // true
@@ -59,3 +60,41 @@ Person.prototype.species = 'Homo Sapiens';
 * Object.create();  --- the easiest and most straightforward way
 
 */
+
+const Animal = function (name, age) {
+  this.name = name;
+  this.age = age;
+};
+
+const cat = new Animal('cat', 4);
+console.log(cat);
+
+Animal.prototype.setSound = function (str) {
+  this.sound = str;
+  console.log(this);
+};
+cat.setSound('miyawuu');
+
+console.log(Animal);
+
+//- CODING CHALLENGE #1
+
+// 1. create a constructor function to implement a Car
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+// 2. implement an `accelerate` method that will increase the car's speed by 10,
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+};
+// 3. Implement a `brake` method that will decrease the car's speed by 5, and log the new speed to the console.
+Car.prototype.brake = function () {
+  this.speed -= 10;
+  console.log(`the speed of the car is decreased to : ${this.speed} km/h`);
+};
+
+const BMW = new Car('BMW', 130);
+console.log(BMW);
+BMW.brake();
+const Mercedes = new Car('Mercedes', 230);
